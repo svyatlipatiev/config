99986ba179b5b5611b661924aba2b869f29baec7
svyatlipatiev <49938339+svyatlipatiev@users.noreply.github.com>
2019-07-31 15:24:59 +0300

    Update install

--- k8s-inst-init.sh
+++ k8s-inst-init.sh
@@ -3,12 +3,10 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-
 echo "deb http://apt.kubernetes.io kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get install -y docker-ce kubelet kubeadm kubectl
+apt-get install -y docker.io kubelet kubeadm kubectl
 
 kubeadm init --ignore-preflight-errors=all
 mkdir -p /home/azureuser/.kube && rm -f /home/azureuser/.kube/config && sudo cp -rf -i /etc/kubernetes/admin.conf /home/azureuser/.kube/config && sudo chown azureuser:azureuser /home/azureuser/.kube/config
