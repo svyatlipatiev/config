ed33a09686b422c33877fffa7e23386a1e55e72d
svyatlipatiev <49938339+svyatlipatiev@users.noreply.github.com>
2019-07-31 15:27:28 +0300

    update

--- k8s-inst-node.sh
+++ k8s-inst-node.sh
@@ -3,12 +3,10 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-add-apt-repository "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-
 echo "deb http://apt.kubernetes.io kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get install -y docker-ce kubelet kubeadm kubectl
+apt-get install -y docker.io kubelet kubeadm kubectl
 
 
 
