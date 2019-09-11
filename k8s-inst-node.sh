94999f4d779777baa1aa9e07a342701927bf6992
svyatlipatiev <49938339+svyatlipatiev@users.noreply.github.com>
2019-09-11 13:10:35 +0300

    Update k8s-inst

--- k8s-inst-node.sh
+++ k8s-inst-node.sh
@@ -6,7 +6,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
 
 apt-get update
-apt-get install -y docker.io kubelet kubeadm kubectl
+apt-get install -y docker.io kubelet=1.12.7-00 kubeadm=1.12.7-00 kubectl=1.12.7-00
 
 
 
